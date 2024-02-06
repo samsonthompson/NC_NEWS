a4c8d4c29ed47667c63f4fb772441e4348c5dc1c
SamsonThompson <samsonthompson@samsontpsonsair.home>
2024-02-06 12:26:17 +0000

    CORE-view-all-articles

---
+++ src/pages/Topics.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Topics =  () => {
+    return (
+        <div>
+            <h2>
+                TOPICS PAGE
+            </h2>
+        </div>
+    )
+}
+
+export default Topics
